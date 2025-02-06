de481ec072bc3f39bc43c4f4df74b4deddb6cf61
rick-grindsta <rickvanbaalen@icloud.com>
2025-02-06 21:20:44 +0100

    Progress

---
+++ src/main/java/com/tourney/views/TournamentDetails.java
@@ -0,0 +1,7 @@
+package com.tourney.views;
+
+import com.webforj.component.html.elements.Div;
+
+public class TournamentDetails extends Div {
+    
+}
--- src/main/java/com/tourney/views/TournamentView.java
+++ src/main/java/com/tourney/views/TournamentView.java
@@ -3,9 +3,15 @@ package com.tourney.views;
 import com.tourney.models.Tournament;
 import com.tourney.services.TournamentService;
 import com.webforj.component.Composite;
+import com.webforj.component.Expanse;
+import com.webforj.component.html.elements.Div;
 import com.webforj.component.html.elements.H1;
+import com.webforj.component.icons.TablerIcon;
 import com.webforj.component.layout.flexlayout.FlexLayout;
 import com.webforj.component.optiondialog.OptionDialog;
+import com.webforj.component.tabbedpane.Tab;
+import com.webforj.component.tabbedpane.TabbedPane;
+import com.webforj.component.tabbedpane.event.TabSelectEvent;
 import com.webforj.environment.ObjectTable;
 import com.webforj.router.NavigationContext;
 import com.webforj.router.annotation.Route;
@@ -17,15 +23,29 @@ import com.webforj.router.observer.DidEnterObserver;
 @Route(value = "/tournament/:id", outlet = MainLayout.class)
 public class TournamentView extends Composite<FlexLayout> implements DidEnterObserver, HasFrameTitle {
     private FlexLayout self = getBoundComponent();
+    private TabbedPane pane;
     private Tournament tournament;
     private TournamentService tournamentService = new TournamentService();
 
     public TournamentView() {
-        
-    }
+        self.setHeight("100%");
 
-    private void redraw() {
-        
+        pane = new TabbedPane().addClassName("tournament-tab");
+        pane.setWidth("100%");
+        pane.setHeight("100%");
+        self.add(pane);
+
+        pane.addTab(new Tab("Details", TablerIcon.create("file-description")));
+        pane.setComponentFor(0, new TournamentDetails());
+
+        pane.addTab(new Tab("Participants", TablerIcon.create("users")));
+        pane.setComponentFor(1, new TournamentParticipants());
+
+        pane.addTab(new Tab("Groups", TablerIcon.create("folders")));
+        pane.setComponentFor(2, new TournamentGroups());
+
+        pane.addTab(new Tab("Matches", TablerIcon.create("ladder")));
+        pane.setComponentFor(3, new TournamentMatches());
     }
 
     @Override
@@ -37,7 +57,6 @@ public class TournamentView extends Composite<FlexLayout> implements DidEnterObs
             return;
         } else {
             setAppTitle();
-            redraw();
         }
     }
 
